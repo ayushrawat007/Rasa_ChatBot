a3af11ed1b13faab33b4e6732634f33fdb102096
ayush <ayushrawatmsit@gmail.com>
2019-12-18 17:47:59 +0530

    With proper functions

--- flask/app/views.py
+++ flask/app/views.py
@@ -48,118 +48,125 @@ state={
 def context_processor():
     return dict(key='value')
 
+def openfile(path,permission):
+    with open(path,permission) as f:
+        if path.endswith('.csv'):
+            reader = csv.reader(f)
+            data = list(reader)
+        if path.endswith('.json'):
+            data=json.load(f)
+    f.close()
+    return data
+
+def writefile(obj,path,permission):
+
+    with open(path, permission) as outfile:
+        json.dump(obj, outfile)
+    outfile.close()
+
+def predict_intent(query):
+    nlp = spacy.load("en_core_web_sm")
+    loaded_model=joblib.load('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\model\\model.pkl')
+    prediction=loaded_model.predict(nlp(query).vector.reshape(1,384)) 
+    return prediction
+
+def get_response_object(answer):
+    res = make_response(jsonify({"what_to_ask":answer}), 200)
+    return res
+
+
+def extract_entity(query,entity_type):
+    nlp=spacy.load('en')
+    doc=nlp(query.title())
+    # doc=nlp(details["query"].title())
+    count=0
+    gpe=list()
+    for ent in doc.ents:
+        if ent.label_==entity_type:
+                gpe.append(ent.text)
+                count+=1
+    if count==2:
+        departure=gpe[0]
+        destination=gpe[1]
+        return (departure,destination)
+    return ("not_allowed","")
+    
+
 @chatBot.route("/",methods=["GET","POST"])
 def root():
-    
-    with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airline.csv', 'rt') as f:
-        reader = csv.reader(f)
-        airline_list = list(reader)
+    airline_list=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airline.csv','rt')
 
     session["state"]=0
     session["airline"]=""
     session["list_of_airline"]=airline_list
 
-    print("------------------------------")
-    print(airline_list)
-    
     return render_template('public/template/chatbot.html')
-
-
-
 
 
 @chatBot.route("/chatBot",methods=["GET","POST"])
 def bot_reply():
-
     details = request.get_json() 
+
     if details["query"]=="quit":
         session["state"]=0
         session["airline"]=""
-        res = make_response(jsonify({"what_to_ask":"What else can I help you with ?"}), 200)
-
+        res=get_response_object("What else can I help you with ?")
         return res
 
-
     if session["state"]==0:
-        nlp = spacy.load("en_core_web_sm")
-        loaded_model=joblib.load('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\model\\model.pkl')
-        print("In bot_reply")
-        print(details)
-        prediction=loaded_model.predict(nlp(details["query"]).vector.reshape(1,384)) 
+        prediction=predict_intent(details["query"])
+
         if(str(prediction[0])=="flight_booking"):
             session["state"]=1
             session["intent"]="flight_booking"
-            res = make_response(jsonify({"intent":str(prediction[0]),"what_to_ask":"In which Airline do you want to book the flight ?"}), 200)
+            res=get_response_object("In which Airline do you want to book the flight ?")
             return res
        
 
     if session["state"]==1:
-        nlp = spacy.load("en_core_web_sm")
-        loaded_model=joblib.load('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\model\\model.pkl')
-        prediction=loaded_model.predict(nlp(details["query"]).vector.reshape(1,384))
+        prediction=predict_intent(details["query"])
 
         if(str(prediction[0])=="flight_booking"):
             session["state"]=1
-            res = make_response(jsonify({"intent":str(prediction[0]),"what_to_ask":"In which Airline do you want to book the flight ?"}), 200)
+            res=get_response_object("In which Airline do you want to book the flight ?")
             return res
-        
-        
+
         # Next step we can check if we support this booking for the specified airline or not other wise return to session["state"] 0
-        
+
         if(str(prediction[0])=="bogus"):
             airline_list=list(session["list_of_airline"][0])
             airline=details["query"]
             if airline in airline_list:
                 session["airline"]=airline
                 session["state"]=2
-                res = make_response(jsonify({"what_to_ask":"Please specify the Location date and time for the booking?"}), 200)
+                res=get_response_object("Please specify the Location date and time for the booking?")
                 return res
-            
             airlines_string=','.join(airline_list)
-            print(airlines_string)
-            res = make_response(jsonify({"what_to_ask":"We don't server this Airline.Please select from the following.\n" + airlines_string }), 200)
+            res=get_response_object("We don't server this Airline.Please select from the following.\n" + airlines_string)
             return res
     
     if session["state"]==2:
-        nlp=spacy.load('en')
-        
-        doc=nlp(details["query"].title())
-        count=0
-        gpe=list()
-        for ent in doc.ents:
-            if ent.label_=="GPE":
-                gpe.append(ent.text)
-                count+=1
-        if count==2:
-            session["departure"]=gpe[0]
-            session["destination"]=gpe[1]
+        if extract_entity(details["query"],"GPE")[0]!="not_allowed":
+            session["departure"],session["destination"]=extract_entity(details["query"],"GPE")
         else:
-            res = make_response(jsonify({"what_to_ask":"Please specify the Location correctly for the booking?"}), 200)
+            res=get_response_object("Please specify the Location correctly for the booking?")
             return res
 
-        print(session["departure"],session["destination"])
-
         if(session["airline"]=="indigo"):
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json') as json_file:
-                data = json.load(json_file)
-                print(data)
+            data=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json','rt')
 
         elif session["airline"]=="airasia":
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json') as json_file:
-                data = json.load(json_file)
+            data=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json','rt')
         
         retur_str=""
 
         for flight in data:
-            
-            print(data[flight]["departure"],data[flight]["destination"])
             if session["departure"]==data[flight]["departure"] and session["destination"] == data[flight]["destination"]:
                 retur_str=flight+" , Fare->"+data[flight]["charge"]+" Time->"+data[flight]["time"]
 
         if retur_str=="":
             with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json') as json_file:
                 unanswere_query = json.load(json_file)
-            print(unanswere_query)
             if session["intent"] not in unanswere_query:
                 unanswere_query[session["intent"]]={}
             if session["airline"] not in unanswere_query[session["intent"]]:
@@ -171,25 +178,15 @@ def bot_reply():
 
             })
 
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json', 'w') as outfile:
-                json.dump(unanswere_query, outfile)
-           
-            session["state"]=1
+            session["state"]=2
             
+            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json', 'w') as outfile:
+                json.dump(unanswere_query, outfile)    
             res = make_response(jsonify({"what_to_ask":"There are no flights available for this route.What else can I help you with?\n" }), 200)
             return res
 
         res = make_response(jsonify({"what_to_ask":"-------------Following flights are available in "+session["airline"]+"------------\n" + retur_str }), 200)
         return res
-
-
-
-        
-            
-        
-
-        
-
 
         #Extract the to and from from regex and check for date ,day and time 
         #if not present ask for the same :
@@ -198,7 +195,6 @@ def bot_reply():
         session["state"]==0
         return res
 
-    
     res=res = make_response(jsonify({"what_to_ask":"I dont understand"}), 200)
     return res 
 
@@ -206,52 +202,29 @@ def bot_reply():
 
 @chatBot.route("/dashboard",methods=["GET","POST"])
 def data_fill():
-    with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json') as json_file:
-        uq = json.load(json_file)
-
+    uq=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json','rt')
     if request.method == "POST":
         details = request.form
-        print(details)
-        print(details["unansweredquery"])
-        print(details["answer"])
-
         answer_list=details["answer"].split(",")
-        print(answer_list)
-        
         nlp=spacy.load('en')
-        print(details["unansweredquery"])
         doc=nlp(details["unansweredquery"].title())
-            
         print("----------------before json load -----------------------")
         if details["airline"]=='indigo':
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json') as json_file:
-                airline_data = json.load(json_file)
+            airline_data=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json','rt')
         if details["airline"]=='airasia':
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json') as json_file:
-                airline_data = json.load(json_file)
-        count=0
+            airline_data=openfile('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json','rt')
+        
         print("----------------before entity extraction -----------------------")
-        gpe=[]
-        for ent in doc.ents:
-            if ent.label_=="GPE":
-                gpe.append(ent.text)
-                count+=1
-        print("----------------before json load -----------------------")
-        if count==2:
-            
-            departure=gpe[0]
-            destination=gpe[1]
-            print("---------entity  extracted----------")
 
+        if extract_entity(details["unansweredquery"],"GPE")[0]!="not_allowed":
+            departure,destination=extract_entity(details["unansweredquery"],"GPE")
+            print("---------entity  extracted----------")
             airline_data[answer_list[0]]={
                 "departure": departure,
                 "destination": destination,
                 "time": answer_list[1],
                 "charge":answer_list[2] 
             }
-            
-            print("----------------------------------------------------------------------------")
-            print(airline_data)
             try:
                 for queries in uq[details["intent"]][details["airline"]]:
                     if queries["query"]==details["unansweredquery"]:
@@ -259,60 +232,12 @@ def data_fill():
             except KeyError:
                 print("----------------------Key Error Occured-----------------")
                 pass
+        
             print("----------------before json dump  -----------------------")
             if details["airline"]=="indigo":
-                with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json', 'w') as outfile:
-                    json.dump(airline_data, outfile)
-
+                writefile(airline_data,'G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\indigo.json','w')
             if details["airline"]=="airasia":
-                with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json', 'w') as outfile2:
-                    json.dump(airline_data, outfile2)
-            
-            with open('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json', 'w') as outfile3:
-                    json.dump(uq, outfile3)
-
-
+                writefile(airline_data,'G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\airasia.json','w')
+            writefile(uq,'G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\data\\unanswered_queries.json','w')
+        
     return render_template('public/template/dashboard.html',uq=uq)
-
-
-    
-    # details = request.get_json() 
-    # if details["query"]=="quit":
-    #     session["state"]=0
-    #     res = make_response(jsonify({"what_to_ask":"What else can I help you with ?"}), 200)
-    #     return res
-    # if session["state"]==0:
-    #     nlp = spacy.load("en_core_web_sm")
-    #     loaded_model=joblib.load('G:\\Users\\Ayush\\PycharmProjects\\Real_Project_ChatBot\\flask\\app\\model\\model.pkl')
-    #     print("In bot_reply")
-    #     print(details)
-    #     prediction=loaded_model.predict(nlp(details["query"]).vector.reshape(1,384)) 
-    #     if(str(prediction[0])=="flight_booking"):
-    #         session["state"]=1
-    #         res = make_response(jsonify({"intent":str(prediction[0]),"what_to_ask":"In which Airline do you want to book the flight ?"}), 200)
-    #         return res
-    
-    # if session["state"]==1:
-    #     airline=details["query"]
-    #     # Next step we can check if we support this booking for the specified airline or not other wise return to session["state"] 0
-    #     session["state"]=2
-    #     res = make_response(jsonify({"what_to_ask":"Please specify the Location date and time for the booking?"}), 200)
-    #     return res
-    # if session["state"]==2:
-    #     #Extract the to and from from regex and check for date ,day and time 
-    #     #if not present ask for the same :
-    #     #if it is a invalid request return to the 1 session["state"] and ask to all the details again
-    #     res = make_response(jsonify({"what_to_ask":"Booking done ......Thank you for your time you can download the tickets from the following link www.random.com"}), 200)
-    #     session["state"]==0
-    #     return res
-    
-
-
-
-   
-
-    
-
-
-
-
